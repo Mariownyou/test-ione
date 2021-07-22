187601ca4c4b55049234fe72e402056ee5fe93d8
Leo Kondratev <39639000+Mariownyou@users.noreply.github.com>
2021-07-23 00:13:46 +0300

    начал!

---
+++ components/TaskCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <a :href="task.id" class="hover:bg-light-blue-500 hover:border-transparent hover:shadow-lg group block rounded-lg p-4 border border-gray-200">
+        <dl class="grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-center">
+            <div>
+                <dt class="sr-only">Title</dt>
+                <dd class="group-hover:text-white leading-6 font-medium text-black">
+                    {{ task.title }}
+                </dd>
+            </div>
+            <div>
+                <dt class="sr-only">Completedy</dt>
+                <dd class="group-hover:text-light-blue-200 text-sm font-medium sm:mb-4 lg:mb-0 xl:mb-4">
+                    {{ task.userId }}
+                </dd>
+            </div>
+            <div class="col-start-2 row-start-1 row-end-3">
+                <dt class="sr-only">Users</dt>
+                <!-- <dd class="flex justify-end sm:justify-start lg:justify-end xl:justify-start -space-x-2">
+                    <img x-for="user in item.users" :src="user.avatar" :alt="user.name" width="48" height="48" class="w-7 h-7 rounded-full bg-gray-100 border-2 border-white" />
+                </dd> -->
+            </div>
+        </dl>
+    </a>
+</template>
+
+<script>
+export default {
+    props: {
+        task: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
---
+++ components/TasksList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mt-10 w-100">
+    <h2 class="text-2xl tracking-tight font-bold text-gray-900 sm:text-2xl md:text-3xl">Tasks</h2>
+    
+    <div 
+    class="grid grid-flow-row grid-cols-2 gap-4" >
+        <TaskCard 
+        v-for="task of tasks" 
+        :key=task.id 
+        :task=task />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+const tasksUrl = 'https://jsonplaceholder.typicode.com/todos'
+
+export default {
+    data() {
+        return {
+            tasks: []
+        }
+    },
+
+    async created() {
+        try {
+            const res = await axios.get(tasksUrl)
+            this.tasks = res.data
+        } catch(e) {
+            this.tasks = [{id: 1, title: 'Error'}]
+        }
+    }
+}
+</script>
